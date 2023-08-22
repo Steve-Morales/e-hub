2fc48423f592a8169a4269a6ef884ee005612998
Steve-Morales <sam008@ucsd.edu>
2023-08-21 22:37:21 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="">
+
+    
+    {/* User Card */}
+    <div className="flex flex-col items-center justify-center w-fit m-4 border-2 rounded-xl border-indigo-600">
+      {/* User Image */}
+      <div className="fixed -top-9 w-fit border-2 rounded-full border-indigo-600">
+        <img className="rounded-full" src="https://placehold.co/90x90"/>
+      </div>
+      
+      <div>
+        <img src="https://placehold.co/325x300" className="rounded-xl"/>
+      </div>
+      <div>
+        <h3 className="font-extrabold">Jhon Doe</h3>
+      </div>
+
     </div>
+    </div>
+    </>
   );
 }
 

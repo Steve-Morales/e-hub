c575adc903e1d99b03d8ac6984e18faa68a8d326
Steve-Morales <sam008@ucsd.edu>
2023-08-22 12:23:32 -0700

    Added Blur Effect Behind Card Img

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -2,44 +2,52 @@
 import React, { useState } from "react";
 
 export default function UserCard() {
-  const [user, setUser] = useState({
-    name: "John Doe",
-    profile_picture: "https://placehold.co/90x90",
-    profile_img: "https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2018/05/Gradient-Round-Up-Featured-Image.jpg?w=960&h=600&crop=1",
-  });
+    const [user, setUser] = useState({
+        name: "John Doe",
+        profile_picture: "https://placehold.co/90x90",
+        profile_img: "sample_snapshot.png",
+    });
 
 
-  return (
-    <div className="relative">
-      {/* Spacer for User Image */}
-      <div className="h-5"></div>
+    return (
+        <div className="relative">
+            {/* Spacer for User Image */}
+            <div className="h-5"></div>
 
-      {/* User Card */}
-      <div
-        className="flex flex-col items-center justify-center w-fit m-4 border-2 rounded-xl border-indigo-600"
-      >
-        {/* User Image */}
-        <div className="absolute top-0 w-fit border-2 rounded-full border-indigo-600 bg-white">
-          <img
-            className="rounded-full object-contain"
-            src={user.profile_picture}
-            alt="User"
-            style={{ maxWidth: "90px", maxHeight: "90px" }}
-          />
+            {/* User Card */}
+            <div
+                className="flex flex-col items-center justify-center w-fit m-4 border-2 rounded-xl border-indigo-600"
+            >
+                <div className="bg-white rounded-xl p-4 relative">
+                    <div className="absolute inset-0 z-0">
+                        <img
+                            src={user.profile_img}
+                            className="w-full h-full object-cover rounded-xl blur-sm"
+                            alt="User Card"
+                        />
+                    </div>
+                    <div className="relative z-1">
+                        <img
+                            src={user.profile_img}
+                            className="rounded-xl object-contain"
+                            alt="User Card"
+                            style={{ maxWidth: "325px", maxHeight: "300px", minWidth: "325px", minHeight: "300px" }}
+                        />
+                    </div>
+                </div>
+                {/* User Image */}
+                <div className="absolute top-0 w-fit border-2 rounded-full border-indigo-600 bg-white">
+                    <img
+                        className="rounded-full object-contain"
+                        src={user.profile_picture}
+                        alt="User"
+                        style={{ maxWidth: "90px", maxHeight: "90px" }}
+                    />
+                </div>
+                <div>
+                    <h3 className="text-xl font-extrabold">{user.name}</h3>
+                </div>
+            </div>
         </div>
-{/* TODO: add blur to image and make object-contain */}
-        <div>
-          <img
-            src={user.profile_img}
-            className="rounded-xl object-fill"
-            alt="User Card"
-            style={{ maxWidth: "325px", maxHeight: "300px", minWidth: "325px", minHeight: "300px" }}
-          />
-        </div>
-        <div>
-          <h3 className="font-extrabold">{user.name}</h3>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }

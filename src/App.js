4d15334510b3783aa766790301f1ad6915f1c94c
Steve-Morales <sam008@ucsd.edu>
2023-08-23 12:09:52 -0700

    Updated Profile page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Users from './views/Users';
 import Home from './views/Home';
+import Profile from './views/Profile';
 
 function App() {
   return (
     <>
     {/* <Users/> */}
-    <Home/>
+    {/* <Home/> */}
+    <Profile/>
     </>
   );
 }
--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,12 +1,25 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-export default function Profile()
-{
-    return(
+export default function Profile() {
+    return (
         <>
-        <Navbar/>
-        <Footer/>
+            <Navbar />
+            <div className="relative flex flex-col items-center justify-center">
+                <img
+                    className="w-4/6 object-contain"
+                    src="https://placehold.co/1920x960"
+                />
+                <div className="absolute bottom-0 left-0 w-fit border-2 rounded-full border-gray-600 bg-white">
+                    <img
+                        className="rounded-full object-contain"
+                        src="https://placehold.co/220x220"
+                        alt="User"
+                        style={{ maxWidth: "220px", maxHeight: "220px" }}
+                    />
+                </div>
+            </div>
+            <Footer />
         </>
     );
 }

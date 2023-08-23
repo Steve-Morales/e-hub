a4166084f11f366eb27e8d0f5f76476cff873327
Steve-Morales <sam008@ucsd.edu>
2023-08-22 23:36:28 -0700

    Added Sections to homepage

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,8 @@
-import './Button.css';
-
 export default function Button({btn_text}) {
     return (
         <>
             <button
-                className='hover:bg-gray-600 transition duration-300 ease-in-out bg-gray-800 text-white text-center max-w-sm w-full p-4 rounded-sm text-xl'
+                className='hover:bg-gray-600 transition duration-800 ease-in-out bg-gray-800 text-white text-center max-w-sm w-full p-4 rounded-sm text-xl'
             >
                 {btn_text ? btn_text : "Button"}
             </button>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -9,13 +9,23 @@ export default function Home() {
         <>
             <Navbar />
             
-            <section className='m-4 flex flex-col items-center justify-center p-4'>
+            <section className='m-4 flex flex-col items-center justify-center p-16'>
                 <div className='w-full h-8'>{/* SPACER */}</div>
                 <h1 className='text-6xl font-extrabold'>Simple. Better. Faster.</h1>
                 <div className='w-full h-8'>{/* SPACER */}</div>
                 <subtitle className="text-xl text-stone-400 w-3/6 p-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi placeat impedit enim sapiente eos quis, praesentium perspiciatis nisi officia illo eaque obcaecati quibusdam nulla sit id eius cumque tempore repellendus.</subtitle>
                 <div className='w-full h-8'>{/* SPACER */}</div>
                 <Button btn_text={"Sample Button"}/>
+            </section>
+
+            <section className='grid grid-row-4 gap-4 p-16 bg-gray-800'>
+                <div className='grid grid-cols-10 gap-2'>
+                    <h1 className='col-span-4 text-6xl font-extrabold text-white'>Networking Displays Made Easy</h1>
+                </div>
+
+                <div className='grid grid-cols-10 gap-2'>
+                    <subtitle className="col-span-6 text-xl text-stone-400 p-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi placeat impedit enim sapiente eos quis, praesentium perspiciatis nisi officia illo eaque obcaecati quibusdam nulla sit id eius cumque tempore repellendus.</subtitle>
+                </div>
             </section>
 
             <Footer />

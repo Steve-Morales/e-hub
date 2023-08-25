e31187e8d45d5879fbd2677b89e12cfca99d48d8
mjalbert99 <67979651+mjalbert99@users.noreply.github.com>
2023-08-25 15:48:35 -0700

    background image too large

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -5,6 +5,13 @@ export default function Login(){
     return(
         <>
             <Navbar />
+            
+            <div className="flex flex-col items-center justify-center w-screen h-[calc(100vh-3rem)] bg-[url('https://placehold.co/1920x1080')]">
+
+                <div className="bg-blue-500 border border-transparent rounded-lg shadow-md w-2/3 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 h-3/5 sm:h-2/3">
+                    .
+                </div>
+            </div>
         </>
     )
 }

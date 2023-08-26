639fe312e9a5396ae6eb4a1e46b79bacd3a114ee
mjalbert99 <67979651+mjalbert99@users.noreply.github.com>
2023-08-26 16:17:40 -0700

    fixed background stretched bug

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -6,7 +6,7 @@ export default function Login(){
         <>
             <Navbar />
             
-            <div className="flex flex-col items-center justify-center w-screen h-[calc(100vh-3rem)] bg-[url('https://placehold.co/1920x1080')]">
+            <div className="flex flex-col items-center justify-center w-screen h-[calc(100vh-3rem)] bg-[url('https://placehold.co/1920x1080')] bg-cover">
 
                 <div className="bg-blue-500 border border-transparent rounded-lg shadow-md w-2/3 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 h-3/5 sm:h-2/3">
                     .

6b399e1e34c478e777e9ad30a54b1185f8d69034
Steve-Morales <sam008@ucsd.edu>
2023-08-26 20:31:04 -0700

    Login Page Completed

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,4 +1,4 @@
-
+// TODO: update link for register
 import Navbar from "../components/Navbar";
 
 export default function Login(){
@@ -8,7 +8,7 @@ export default function Login(){
             
             <div className="flex flex-col items-center justify-center w-screen h-[calc(100vh-3rem)] bg-[url('https://placehold.co/1920x1080')] bg-cover">
 
-                <div className="bg-blue-500 border border-transparent rounded-lg shadow-md w-2/3 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 h-3/5 sm:h-2/3 flex flex-col items-center justify-center">
+                <div className="bg-gray-800 border border-transparent rounded-lg shadow-md w-2/3 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 h-3/5 sm:h-2/3 flex flex-col items-center justify-center">
 
                     <form className="rounded flex flex-col items-start justify-center">
                         <div className="mb-8">
@@ -27,11 +27,12 @@ export default function Login(){
                             </input>
                             <a className="text-sm text-blue-300 font-bold  hover:text-blue-100 transition-colors duration-300" href="/#">forgot password?</a>
                         </div>
-                        <div className="mb-1">
-                            <button className="rounded bg-gray-800 text-gray-300 font-bold hover:bg-gray-500 transition-colors duration-300 px-10 py-2" type="button">Log-In</button>
+                        <div className="mb-1 w-full flex flex-col items-center justify-center">
+                            <button className="border-2 border-indigo-400 rounded bg-gray-950 text-gray-300 font-bold hover:bg-gray-500 transition-colors duration-300 px-10 py-2" type="button">Log-In</button>
                         </div>
                         <div className="mb-3">
-                            <text className="text-gray-800 text-xs font-bold">New to e-hub? </text>
+                            <text className="text-gray-300 text-xs font-bold">New to e-hub? </text>
+                            {/* FIXME: update link here! */}
                             <a href="/#" className="text-sm text-blue-300 font-bold hover:text-blue-100 transition-colors duration-300">Register</a>
                         </div>
                     </form>
